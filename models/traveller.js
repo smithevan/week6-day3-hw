c674c81204b4266a0fc533b921c64a4ffa805fb4
Evan Smith <smith.m.evan@gmail.com>
2020-01-08 16:59:44 +0000

    start locations passing

---
+++ models/traveller.js
@@ -0,0 +1,37 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const startLocations = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return startLocations; 
+  // const filmTitles = this.films.map((film) => {
+  //   return film.title;
+  // });
+  // return filmTitles;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
